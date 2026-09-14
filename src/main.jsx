1f3921ff51c4fe71db36d379870c57c17f386c97
Pro <jirattha2012@gmail.com>
2026-09-14 15:10:30 +0700

    close

--- src/main.jsx
+++ src/main.jsx
@@ -313,8 +313,8 @@ function App() {
                   <p className="question">แต่ถ้าเธออยากบอกอะไรกับเรา…</p>
                   <div className="response-buttons">
                     {/* <button className="primary" onClick={() => chooseResponse("talk")}> */}
-                    {/* <button className="primary" onClick={() => setShowMaintenance(true)}> */}
-                    <button className="primary" onClick={handleTalkClick}>
+                    <button className="primary" onClick={() => setShowMaintenance(true)}>
+                    {/* <button className="primary" onClick={handleTalkClick}> */}
                       ลองคุยกันอีกครั้งนะ <Heart size={17}/>                    
                     </button>
                     
